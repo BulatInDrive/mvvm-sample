4f7cffb587ec574e08203581d5847e99b84040d6
BulatMukhutdinov <Bulatmm@yandex.ru>
2025-02-07 18:30:51 +0500

    Fix crash

--- app/src/main/java/tat/mukhutdinov/mvvm/form/ui/FormViewModel.kt
+++ app/src/main/java/tat/mukhutdinov/mvvm/form/ui/FormViewModel.kt
@@ -5,27 +5,14 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import tat.mukhutdinov.mvvm.form.domain.model.Points
 
-class FormViewModel(
-    private val mediator: FormMediator
-) : ViewModel() {
+class FormViewModel : ViewModel() {
 
     private val _isSearchingRide = MutableStateFlow(false)
     val isSearchingRide: StateFlow<Boolean> = _isSearchingRide.asStateFlow()
 
-    private val _selectedPoints = MutableStateFlow(Points())
-    val selectedPoints: StateFlow<Points> = _selectedPoints.asStateFlow()
-
-    fun updateSelectedPoints(points: (Points) -> Points) {
-        _selectedPoints.update(points)
-    }
-
     fun searchRide() {
-        mediator.selectedPoints.value
         _isSearchingRide.update { true }
-
-
     }
 
     fun cancelRideSearch() {
